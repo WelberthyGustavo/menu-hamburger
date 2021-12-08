07f0d4f4fecd2cd33d46bd2f7b23e3f5613e2b49
Welberthy Gustavo <54287781+WelberthyGustavo@users.noreply.github.com>
2021-12-08 14:33:59 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Menuzinho</title>
+</head>
+<body>
+    <header id="header">
+        <a id="logo" href="">Logo</a>
+        <nav id="nav">
+            <button id="btn-mobile" aria-label="Abrir menu" aria-haspopup="true" aria-controls="menu" aria-expanded="false">Menu
+                <span id="hamburger"></span>
+            </button>
+            <ul id="menu" role="menu">
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About us</a></li>
+                <li><a href="/">Softwares</a></li>
+                <li><a href="/">Sing Up</a></li>
+            </ul>
+        </nav>
+    </header>
+    <script src="menu.js"></script>
+</body>
+</html>
